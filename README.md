f236c56e813c2ca23628dffa33e94956cc024e86
ariyan bin layek <ariyan.bin@gmail.com>
2019-06-21 21:23:09 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+<!--Babel -->
+this is simple of example of babel transpiling from es6 to es5
---
+++ app.js
@@ -0,0 +1,2 @@
+const sum5 = inputNumber => inputNumber + 5;
+console.log(`The sum of 39 and 5 is ${sum5(39)}!`);
